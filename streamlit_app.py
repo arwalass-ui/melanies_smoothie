48c385b3eda1a6c30f1d9ebed89de8521e5d5194
arwalass-ui <arwalass@gmail.com>
2026-09-05 12:49:56 +0200

    Update streamlit_app.py
    ajout colonne search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,10 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 #choisir depuis une table
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe (data=my_dataframe,, use_container_width=True)
+st.stop()
 
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_column = [
     row["FRUIT_NAME"]
