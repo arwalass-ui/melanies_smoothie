40864d1fa2b3dc3fda856a6c71d35ec8c2bc7f11
arwalass-ui <arwalass@gmail.com>
2026-09-05 12:55:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,9 +20,13 @@ session = cnx.session()
 
 #choisir depuis une table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe (data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe (data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#convert the snowpark dataframe to pandas
+pd_df = my_dataframe.to_pandas()
+st.dataframe (pd_df)
+st.stop()
 
 ingredients_column = [
     row["FRUIT_NAME"]
